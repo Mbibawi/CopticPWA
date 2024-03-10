977f8305d1bae8af7281b190b2c5119aa1c9c3f8
Mina BIBAWI <minabibawi@gmail.com>
2024-03-10 14:36:40 +0100

    v5.7.0.1

--- modules/DeclareGlobalVariables.ts
+++ modules/DeclareGlobalVariables.ts
@@ -24,7 +24,7 @@ type typeButton = {
 };
 //CONSTANTS
 const version: string =
-  "v5.7.0 (Worked on Holy Week structure and Litanies + some CSS)";
+  "v5.7.0.1 (Correction to font size of main page and side bar buttons)";
 const calendarDay: number = 24 * 60 * 60 * 1000; //this is a day in milliseconds
 const containerDiv: HTMLDivElement = document.getElementById(
   "containerDiv") as HTMLDivElement;
